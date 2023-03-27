6dd05abc9b175c65b4f8a6abb7cddf25da830850
samjwu <sjwu@ualberta.ca>
2023-03-26 19:41:02 -0600

    add CustomImageDataset class

--- dataset.py
+++ dataset.py
@@ -1,11 +1,32 @@
 import matplotlib.pyplot
 import os
-import pandas as pd
+import pandas
 import torch
 
-from torch.utils.data import Dataset
+from torch.utils.data import DataLoader, Dataset
 from torchvision import datasets
-from torchvision.transforms import ToTensor, Lambda
+from torchvision.io import read_image
+from torchvision.transforms import Lambda, ToTensor
+
+class CustomImageDataset(Dataset):
+    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
+        self.img_labels = pandas.read_csv(annotations_file)
+        self.img_dir = img_dir
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self):
+        return len(self.img_labels)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
+        image = read_image(img_path)
+        label = self.img_labels.iloc[idx, 1]
+        if self.transform:
+            image = self.transform(image)
+        if self.target_transform:
+            label = self.target_transform(label)
+        return image, label
 
 training_data = datasets.FashionMNIST(
     root="data",
@@ -43,3 +64,15 @@ for i in range(1, cols * rows + 1):
     matplotlib.pyplot.axis("off")
     matplotlib.pyplot.imshow(img.squeeze(), cmap="gray")
 matplotlib.pyplot.show()
+
+train_dataloader = DataLoader(training_data, batch_size=64, shuffle=True)
+test_dataloader = DataLoader(test_data, batch_size=64, shuffle=True)
+
+train_features, train_labels = next(iter(train_dataloader))
+print(f"Feature batch shape: {train_features.size()}")
+print(f"Labels batch shape: {train_labels.size()}")
+img = train_features[0].squeeze()
+label = train_labels[0]
+matplotlib.pyplot.imshow(img, cmap="gray")
+matplotlib.pyplot.show()
+print(f"Label: {label}")
